86d5efd1212fdea953025745058d97b01431cd73
Jeffry James Paner <118299402+oddsdefier@users.noreply.github.com>
2024-10-25 16:43:25 +0800

    Refactor image path in SchoolEmblem component

--- src/pages/about/SchoolEmblem.tsx
+++ src/pages/about/SchoolEmblem.tsx
@@ -8,7 +8,7 @@ const SchoolEmblem: React.FC = () => {
 				<div className="relative container mx-auto max-w-4xl">
 					<h2 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-semibold text-clarc-blue">School Emblem</h2>
 					<div className="flex justify-center items-center w-full">
-						<img src="../public/school-emblem.jpg" alt="School Emblem" className="max-w-4xl min-w-full" />
+						<img src="../school-emblem.jpg" alt="School Emblem" className="max-w-4xl min-w-full" />
 					</div>
 				</div>
 
