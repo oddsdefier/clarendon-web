2181b8587abf3781d51703a65d7b370ac0d2daa8
Jeffry James Paner <118299402+oddsdefier@users.noreply.github.com>
2024-11-05 16:07:02 +0800

    Refactor SchoolEmblem component layout; adjust padding and structure for improved styling

--- src/pages/about/SchoolEmblem.tsx
+++ src/pages/about/SchoolEmblem.tsx
@@ -6,17 +6,19 @@ import CustomBreadcrumb from "@/components/BreadCrumb";
 const SchoolEmblem: React.FC = () => {
   const pageContent = (
     <div className="no-scrollbar min-h-svh flex-grow lg:h-[calc(100vh-2rem)] lg:overflow-y-auto lg:px-16">
-      <div className="px-6 lg:px-0">
+      <div className="lg:px-0">
         <CustomBreadcrumb
           currentPage="School Emblem"
           secondLink="/about"
           secondTitle="About"
         />
-        <h2 className="text-xl font-semibold text-clarc-blue sm:text-xl md:text-2xl lg:text-3xl">
-          School Emblem
-        </h2>
-        <div className="flex w-full items-center justify-center">
-          <img src="../school-emblem.jpg" alt="School Emblem" />
+        <div className="px-6 lg:px-0">
+          <h2 className="text-xl font-semibold text-clarc-blue sm:text-xl md:text-2xl lg:text-3xl">
+            School Emblem
+          </h2>
+          <div className="flex w-full items-center justify-center">
+            <img src="../school-emblem.jpg" alt="School Emblem" />
+          </div>
         </div>
       </div>
     </div>
